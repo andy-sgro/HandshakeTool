f35f1b6dceb9e0b37798355b17348a20ad0752bc
uberAquarium <43046839+andy-sgro@users.noreply.github.com>
2021-04-05 10:38:49 -0400

    recreating the forms as user controls

--- HandshakeTool/HandshakeTool/MainPage.Designer.cs
+++ HandshakeTool/HandshakeTool/MainPage.Designer.cs
@@ -42,6 +42,8 @@ namespace HandshakeTool
 			this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.machineLearningToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
 			this.menuStrip1.SuspendLayout();
 			this.tabControl.SuspendLayout();
 			this.tabPage1.SuspendLayout();
@@ -93,6 +95,8 @@ namespace HandshakeTool
 			// 
 			// tabPage1
 			// 
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.label2);
 			this.tabPage1.Controls.Add(this.label1);
 			this.tabPage1.Location = new System.Drawing.Point(4, 25);
 			this.tabPage1.Name = "tabPage1";
@@ -149,6 +153,24 @@ namespace HandshakeTool
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Number of Photos";
 			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(31, 111);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(105, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Time per Photo";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(31, 205);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(105, 34);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Take Photos";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
 			// MainPage
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -185,5 +207,7 @@ namespace HandshakeTool
 		private System.Windows.Forms.ToolStripMenuItem modeToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem machineLearningToolStripMenuItem;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
 	}
 }
